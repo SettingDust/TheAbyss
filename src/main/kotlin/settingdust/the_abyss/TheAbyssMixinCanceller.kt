9d9ae02caaff87051fb2be661798e1cbe9f6b7fe
SettingDust <settingdust@gmail.com>
2024-10-14 19:27:36 +0800

    fix: invert the mixin cancel

--- src/main/kotlin/settingdust/the_abyss/TheAbyssMixinCanceller.kt
+++ src/main/kotlin/settingdust/the_abyss/TheAbyssMixinCanceller.kt
@@ -4,7 +4,7 @@ import com.bawnorton.mixinsquared.api.MixinCanceller
 
 class TheAbyssMixinCanceller : MixinCanceller {
     override fun shouldCancel(targetClassNames: MutableList<String>, mixinClassName: String): Boolean {
-        if (mixinClassName == "com.ishland.c2me.opts.worldgen.vanilla.mixin.aquifer.MixinNoiseChunkGenerator") return false
-        return true
+        if (mixinClassName == "com.ishland.c2me.opts.worldgen.vanilla.mixin.aquifer.MixinNoiseChunkGenerator") return true
+        return false
     }
 }
